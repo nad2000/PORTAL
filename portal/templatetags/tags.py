585c976f230360eb9eff61256b9ff7c634b3d6a9
Rad Cirskis <nad2000@gmail.com>
2024-05-17 14:08:48 +1200

    coloured borders too

--- portal/templatetags/tags.py
+++ portal/templatetags/tags.py
@@ -17,6 +17,22 @@ from .. import models as m
 register = template.Library()
 
 
+@register.filter(is_safe=True)
+def color_variant(hex_color, brightness_offset=1):
+    """takes a color like #87c95f and produces a lighter or darker variant"""
+    if len(hex_color) != 7:
+        raise Exception(
+            "Passed %s into color_variant(), needs to be in #87c95f format." % hex_color
+        )
+    rgb_hex = [hex_color[x : x + 2] for x in [1, 3, 5]]
+    new_rgb_int = [int(round(int(hex_value, 16) * brightness_offset)) for hex_value in rgb_hex]
+    new_rgb_int = [
+        min([255, max([0, i])]) for i in new_rgb_int
+    ]  # make sure new values are between 0 and 255
+    # hex() produces "0x88", we want just "88"
+    return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
+
+
 @register.filter
 def index(indexable, i):
     return indexable[i]
