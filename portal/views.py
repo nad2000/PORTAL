ef9c37158c379ef3e304560a2cc720c9c662c2fe
Rad Cirskis <nad2000@gmail.com>
2024-06-20 20:51:56 -0400

    fixed headers for applicaiton with rich text summary

--- portal/views.py
+++ portal/views.py
@@ -7565,7 +7565,6 @@ def application_exported_view(request, number, lang=None):
             f"{settings.STATIC_URL}images/{domain}/alt_logo_small.png"
         )
     elif self.site_id in [4, 5]:
-        logo_path = os.path.join(settings.STATIC_ROOT, f"images/MBIE_logo.jpg")
         logo_1_url = f"{settings.STATIC_URL}images/MBIE_logo.jpg"
         logo_2_url = f"{settings.STATIC_URL}images/RS_logo.jpg"
 
