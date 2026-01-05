f0b3846e3eeb944bc5585757f25bbe31e2d4c237
PMSP App a.k.a. Rad <app@pmspp.prodata.nz>
2026-01-05 19:01:36 +1300

    try with python 3.13

--- manage.sh
+++ manage.sh
@@ -3,7 +3,8 @@ export ENV=${ENV:-local}
 export DJANGO_DEBUG=False
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ -z "$VIRTUAL_ENV" ] ; then
-  [ -f $HOME/venv311/bin/activate ] && source $HOME/venv311/bin/activate
+  [ -f $HOME/venv313/bin/activate ] && source $HOME/venv313/bin/activate || \
+  [ -f $HOME/venv311/bin/activate ] && source $HOME/venv311/bin/activate || \
   [ -f $PWD/venv/bin/activate ] && source $PWD/venv/bin/activate
 fi
 python $SCRIPT_DIR/manage.py "$@"
