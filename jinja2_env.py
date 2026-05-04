89988fb1bde7905f30de01624c8a357bf51177db
Rad Cirskis <nad2000@gmail.com>
2026-05-04 16:32:26 +1200

    deal with jinuja groupby filter limitation

--- jinja2_env.py
+++ jinja2_env.py
@@ -1,16 +1,17 @@
+import itertools
 from os.path import basename
 
-from django.conf import settings
 from crispy_forms.utils import render_crispy_form
+from django.conf import settings
 from django.contrib import messages
-from django.templatetags.static import static
 from django.db.models import Q
+from django.templatetags.static import static
 from django.urls import reverse
 from django.utils import translation
-from jinja2 import Environment, pass_context, BaseLoader, TemplateNotFound, ChoiceLoader
 
 # from dbtemplates.models import Template
 from django.utils.safestring import mark_safe
+from jinja2 import BaseLoader, ChoiceLoader, Environment, TemplateNotFound, pass_context
 
 
 class DbLoader(BaseLoader):
@@ -47,6 +48,11 @@ def summernote(note):
     return note and mark_safe(note.replace(settings.MEDIA_URL, f"file://{settings.MEDIA_ROOT}/"))
 
 
+def sorted_groupby(value, attribute):
+    """Group-by already sorted list by attribute."""
+    return itertools.groupby(value, lambda x: getattr(x, attribute))
+
+
 def environment(loader=None, **options):
     if loader:
         options["loader"] = loader
@@ -78,6 +84,7 @@ def environment(loader=None, **options):
             ),
         }
     )
+    env.filters["sorted_groupby"] = sorted_groupby
     env.filters["basename"] = basename
     env.filters["safe"] = mark_safe
     env.filters["summernote"] = summernote
